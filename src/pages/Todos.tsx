88b2034d77240618794463268e4b6c62473e3727
Mulraj Gupta <mulrajgupta208@gmail.com>
2023-12-08 15:26:08 +0530

    Mutation via Cache(Optimistic updates)

--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,11 +1,12 @@
 import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
 import Nav from "../components/Nav";
 import {ArrowForwardIcon} from '@chakra-ui/icons'
-import {Box, Button, Flex, Input, Spinner, Text} from "@chakra-ui/react";
+import {Box, Button, Input, Text, useToast} from "@chakra-ui/react";
 import {useState} from "react";
 import {API_ENDPOINT} from "../constants.ts";
 
 function Todos() {
+    const toast = useToast()
     const queryClient = useQueryClient()
     const [newTodoText, setTodoText] = useState<string>('');
     const {isLoading, data: todos, error} = useQuery({
@@ -19,9 +20,10 @@ function Todos() {
             })
     });
 
-    const {isPending, variables, mutate, isError} = useMutation({
+    const {mutate} = useMutation({
         mutationFn: (text: string) => {
             setTodoText('')
+
             return fetch(`${API_ENDPOINT}/todo`, {
                 method: 'POST',
                 body: JSON.stringify({text}),
@@ -36,8 +38,36 @@ function Todos() {
                 return res.json()
             })
         },
-        onSettled: async () => {
-            return await queryClient.invalidateQueries({queryKey: ['todos']})
+        onMutate: async (newTodo) => {
+            // Cancel any outgoing refetches
+            // (so they don't overwrite our optimistic update)
+            await queryClient.cancelQueries({queryKey: ['todos']})
+
+            // Snapshot the previous value
+            const previousTodos = queryClient.getQueryData(['todos'])
+
+            // Optimistically update to the new value
+            queryClient.setQueryData(['todos'], () => [...previousTodos, {id: Math.random().toString(), text: newTodo}])
+
+            // Return a context object with the snapshotted value
+            return {previousTodos}
+        },
+        // If the mutation fails,
+        // use the context returned from onMutate to roll back
+        onError: (err, newTodo, context) => {
+            queryClient.setQueryData(['todos'], context.previousTodos)
+
+            toast({
+                title: 'Failed to save the Todo',
+                status: 'error',
+                duration: 9000,
+                isClosable: true,
+            })
+
+            setTodoText(newTodo)
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries({queryKey: ['todos']})
         }
     })
 
@@ -75,24 +105,6 @@ function Todos() {
                         {text}
                     </Text>
                 ))}
-
-                {isPending && (
-                    <Flex align="center">
-                        <Text opacity={0.5} mr={3}>
-                            {variables}
-                        </Text>
-                        <Spinner/>
-                    </Flex>
-                )}
-
-                {isError && (
-                    <Flex align="center">
-                        <Text color="red" mr={3}>{variables}</Text>
-                        <Button onClick={() => mutate(variables)}>
-                            Retry
-                        </Button>
-                    </Flex>
-                )}
             </Box>
         </div>
     )
